ed733c70d8b0feedb12e28f946041c26d065704f
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2018-01-13 01:31:30 +0200

    Custom JavaScript

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+/*
+Add custom JavaScript code here.
+Start the container with a customized version of this file, mounted:
+docker run -dit -p 80:8080 -p 443:8443 -v /path/to/your/custom.js:/var/www/adct/js/custom.js artioml/adct
+*/
